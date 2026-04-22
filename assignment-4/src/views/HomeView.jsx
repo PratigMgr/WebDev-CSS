e201d42fe38ac3286494ef4baa2258a6e6d22013
Pratig Thapa Magar <pratig.thapamagar@dcmail.ca>
2026-04-21 23:29:22 -0400

    Update HomeView.jsx
    change the layout of my home page

--- assignment-4/src/views/HomeView.jsx
+++ assignment-4/src/views/HomeView.jsx
@@ -1,3 +1,7 @@
+//Name: Pratig Thapa Magar
+//Course Code: INFT 2202
+//Date: 2025-04-20
+//Description: This component serves as the home page of the application.
 import React from 'react'
 import { Link } from 'react-router-dom'
 
@@ -5,11 +9,12 @@ export default function HomeView(){
   return (
     //Home page with intro my app and link to list/create.
     <div className="p-4 bg-light rounded">
-      <h2 className="h5">Welcome to Capstone Catalog</h2>
-      <p className="mb-3">Browse, create, and manage your personal collection of items. Each item has a name, category, price, rating, and description.</p>
+      <h2 className="h5"><i>Welcome to Capstone Catalog :- Design by Pratig Thapa</i></h2>
+      <h3 className="h4 mb-2">🏍️ My Favourite Sportbikes Collection</h3>
+      <p className="mb-3 text-muted">Track, compare, and manage your favourite motorbikes. Add specs like engine size, horsepower, and top speed and mark star as favourite, the ones you love most.</p>
       <div className="d-flex gap-2">
-        <Link className="btn btn-outline-secondary" to="/list">Browse List</Link>
-        <Link className="btn btn-primary" to="/new">Add New Item</Link>
+        <Link className="btn btn-outline-secondary" to="/list">Browse Bikes</Link>
+        <Link className="btn btn-primary" to="/new">Add a Bike</Link>
       </div>
     </div>
   )
