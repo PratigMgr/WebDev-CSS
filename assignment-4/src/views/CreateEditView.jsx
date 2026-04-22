0bb6b733713c94eef880e9be05b29738177853b0
Pratig Thapa Magar <pratig.thapamagar@dcmail.ca>
2026-04-21 23:30:45 -0400

    Update CreateEditView.jsx
    I change the ctx with the spcecific one while declaring const

--- assignment-4/src/views/CreateEditView.jsx
+++ assignment-4/src/views/CreateEditView.jsx
@@ -1,3 +1,10 @@
+//Name: Pratig Thapa Magar
+//Course Code: INFT 2202
+//Date: 2025-04-20
+//Description: This component handles both the creation and editing of bike items. 
+// It uses the useParams hook to determine if an ID is present in the URL, which indicates whether the form is in create or edit mode.
+
+
 import React, { useContext } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import ItemForm from '../components/ItemForm'
@@ -6,8 +13,10 @@ import { ItemsContext } from '../context/ItemsContext'
 export default function CreateEditView(){
   const { id } = useParams()
   const navigate = useNavigate()
-  const ctx = useContext(ItemsContext)
+  const { items, addItem, updateItem } = useContext(ItemsContext)
   //initial if editing; onSave add/update then navigate
+    const existing = id ? items.find(i => i.id === id) : null
+
     function handleSave(data){
     if (id) {
       updateItem(id, data)
