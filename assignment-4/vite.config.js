553dee22f2405e0ee26bc3ee38a9e4b3a9332ff6
Pratig Thapa Magar <pratig.thapamagar@dcmail.ca>
2026-04-21 23:28:51 -0400

    Update vite.config.js
    add my introduction

--- assignment-4/vite.config.js
+++ assignment-4/vite.config.js
@@ -1,3 +1,9 @@
+//Name: Pratig Thapa Magar
+//Course Code: INFT 2202
+//Date: 2025-04-20
+//Description: This is the Vite configuration file for the React application. 
+// It imports the necessary plugins and defines the configuration for the Vite build tool.
+// The base path is set to 'WebDev-CSS-Assignment-4' to ensure that the application works correctly when deployed to a subdirectory.
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()] })
+export default defineConfig({ plugins: [react()], base:'WebDev-CSS-Assignment-4' })
